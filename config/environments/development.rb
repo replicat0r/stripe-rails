c1841bede49e047319d815e40f1a9d8e497ab0a2
tyler liu <replicat0r.misc@gmail.com>
2016-04-25 23:16:56 -0400

    added user model and setup devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
+  # Raise an error on page load if there are pending migrations.a
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
@@ -35,7 +35,9 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+  config.action_mailer.default_url_options = {
+    :host => 'localhost:3000'
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
